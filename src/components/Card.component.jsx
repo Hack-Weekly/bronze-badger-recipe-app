e0733f0dc2c59e23a8ad21f0fb9879ebc7aeb3d6
Rmichal503 <rakmichal503@gmail.com>
2023-07-01 12:46:56 +0200

    card props

--- src/components/Card.component.jsx
+++ src/components/Card.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-export const Card = ({cocktailDetails,cocktailName,cocktailImg}) => {
+export const Card = (props) => {
+  const {cocktailDetails,cocktailName,cocktailImg} = props
   return (
-    <div className='m-3 p-5 flex flex-col bg-primary-color w-72 h-80 rounded-lg hover:bg-primary-hover hover:drop-shadow-md'>
+    <div className='m-3 p-5 flex flex-col bg-primary-color w-72 h-80 rounded-lg hover:bg-primary-hover hover:drop-shadow-md' {...props}>
       <img className='h-3/5 rounded-md bg-secondary-color' src={`${cocktailImg ? cocktailImg : '/cocktail.svg'}`}/>
       <div className=' overflow-auto flex flex-col text-primary-bodyText'>
         <h2 className='text-2xl font-bold'>{cocktailName}</h2>
