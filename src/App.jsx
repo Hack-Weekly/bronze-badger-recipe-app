6bbab7e7b8b50ae7c9c556043fbfcd8a2fff9411
cod3-craft3r <awsmarchies@gmail.com>
2023-07-04 11:54:18 +0530

    latest commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import "./App.css";
 import Footer from "./components/Footer.component";
-import Carousel from "./components/Carousel.component";
 
 function App() {
-  const banners=['https://saskatoonfood.ca/images/headers/1000x200-canola-sky.jpg', 'https://saskatoonfood.ca/images/headers/1000x200-pelican2.jpg', 'https://upload.wikimedia.org/wikipedia/commons/5/50/Verhnee_Shavlinskoe_ozero_panorama.jpg'];
   return (
     <>
-      <Carousel images={banners} />
       <main className="flex justify-center items-center w-screen h-screen bg-background"></main>
-      <Footer />
     </>
   );
 }
