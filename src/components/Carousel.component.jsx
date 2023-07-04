0fcec9402096856e4ee6ed90bec327f8ccb7ff46
cod3-craft3r <awsmarchies@gmail.com>
2023-07-04 11:13:27 +0530

    removed conflicting changes

--- src/components/Carousel.component.jsx
+++ src/components/Carousel.component.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect } from "react";
 
 const Carousel = ({ images }) => {
   const [currentImage, setCurrentImage] = useState(0);
