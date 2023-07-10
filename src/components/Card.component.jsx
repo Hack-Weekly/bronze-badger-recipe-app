8e53fc878bdb487ca248d52bec4ed9bcb6592630
Rmichal503 <rakmichal503@gmail.com>
2023-07-10 13:21:20 +0200

    footer responsive size, card tags adj

--- src/components/Card.component.jsx
+++ src/components/Card.component.jsx
@@ -14,10 +14,10 @@ export const Card = ({cocktail}) => {
     <div id='card' className='m-3 p-5 flex flex-col bg-slate-50 dark:bg-cardBg w-72 h-80 rounded-lg drop-shadow-md hover:drop-shadow-2xl dark:drop-shadow-darkMd dark:hover:drop-shadow-dark2xl transition-shadow duration-300'
     onClick={openModal}>
       <img className='h-3/5 w-auto rounded-md bg-secondary-color-light dark:bg-secondary-color-dark' src={`${img ? img : '/cocktail.svg'}`}/>
-      <div className=' overflow-y-auto flex flex-col'>
+      <div className='flex flex-col'>
         <h2 className='font-Marker text-3xl font-bold text-primary-color-light dark:text-secondary-color-dark'>{name}</h2>
-        <p className='overflow-y-auto text-ellipsis text-slate-900 dark:text-primary-text-light'>{tags}</p>
-        <p className='overflow-y-auto text-ellipsis text-slate-900 dark:text-primary-text-light'>{alcohol}</p>
+        <p className='h-fit break-all text-ellipsis text-slate-900 dark:text-primary-text-light'>{tags}</p>
+        <p className='h-fit break-all text-ellipsis text-slate-900 dark:text-primary-text-light'>{alcohol}</p>
       </div>
     </div>
     {stateModal &&
--- src/components/Footer.component.jsx
+++ src/components/Footer.component.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-primary-color-light dark:bg-primary-color-dark py-5 w-screen mt-auto border-t-2 dark:border-secondary-color-dark border-secondary-color-light">
+    <footer className="bg-primary-color-light dark:bg-primary-color-dark py-2 md:py-5 w-screen mt-auto border-t-2 dark:border-secondary-color-dark border-secondary-color-light">
         <div className="flex justify-between items-center">
           <div>
-            <p className="text-primary-text-light dark:text-primary-text-dark mx-4">
+            <p className="text-primary-text-light text-xs md:text-base dark:text-primary-text-dark mx-4">
               To <b>Hack Weekly</b>
             </p>
           </div>
           <div>
-            <p className="text-primary-text-light dark:text-primary-text-dark mx-4">Made by Bronze Badger</p>
+            <p className="text-primary-text-light text-xs md:text-base dark:text-primary-text-dark mx-4">Made by Bronze Badger</p>
           </div>
         </div>
     </footer>
